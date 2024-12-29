3b2fad60f927a9b18cec5aae0fdaba5d2644656b
Jihyeon Park <jihyeona5525@gmail.com>
2024-12-30 05:10:25 +0900

    chore: 로고 수정 및 디자인 세부 수정

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,28 +3,34 @@ import logo from "../../assets/images/logo.png"
 import listIcon from "../../assets/images/list.png"
 import groupIcon from "../../assets/images/group.png"
 import settingsIcon from "../../assets/images/settings.png"
+import logoutIcon from "../../assets/images/logout.png"
 import { Link } from "react-router-dom"
 
 const Sidebar = () => {
+    //로그아웃 시 쿠키 해제 관련 함수 추가
     return (
         <div className={styles.Sidebar}>
             <Link to="/Home" className={styles.logoContainer}>
                 <img src={logo} alt="PinBoard" className={styles.logo} />
             </Link>
-            <Link to="/list" className={styles.settingsLink}>
-                <img src={listIcon} alt="List" className={styles.settingsIcon} />
+            <Link to="/list" className={styles.menuLink}>
+                <img src={listIcon} alt="List" className={styles.menuIcon} />
                 <span>글 리스트</span>
             </Link>
-            <Link to="/group" className={styles.settingsLink}>
-                <img src={groupIcon} alt="Group" className={styles.settingsIcon} />
+            <Link to="/group" className={styles.menuLink}>
+                <img src={groupIcon} alt="Group" className={styles.menuIcon} />
                 <span>그룹 관리</span>
             </Link>
-            <Link to="/user" className={styles.settingsLink}>
-                <img src={settingsIcon} alt="User" className={styles.settingsIcon} />
+            <Link to="/user" className={styles.menuLink}>
+                <img src={settingsIcon} alt="User" className={styles.menuIcon} />
                 <span>계정 설정</span>
             </Link>
+            <Link to="/login" className={styles.logoutLink}>
+                <img src={logoutIcon} alt="User" className={styles.logoutIcon} />
+                <span>로그아웃</span>
+            </Link>
         </div>
-    )
+    ) 
 }
   
 export default Sidebar;
